df2df307683b9635133ec8bc56d8ae678a3c176c
mushrrat Begam <mushrratbegam123@gmail.com>
2026-01-11 23:28:18 +0530

    IF-ELSE-Question

---
+++ Croma_campus/IF-ELSE_Two-Condition.py
@@ -0,0 +1,104 @@
+# 1. Check whether a number is even or odd.
+
+num = int(input("Enter number :"))
+
+if num % 2 ==0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+
+
+
+# 2. find the largest of two number.
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+if a > b:
+    print("Greatest value", a)
+else:
+    print("Greatest value", b)
+
+
+
+# 3. check whether a person eligible for driving license.
+
+age = int(input("Enter age :"))
+
+if age >= 18:
+    print("Eligible for driving license")
+else:
+    print("Not Eligible")
+
+
+
+# 4. print "pass" or "fail" based on marks.
+
+mark = int(input("Enter marks :"))
+
+if mark >=50:
+    print("pass")
+else:
+    print("fail")
+
+
+
+# 5. check whether a number is positive or negative.
+
+num = int(input("Enter number :"))
+
+if num >= 0:
+    print("positive")
+else:
+    print("negative")
+
+
+
+# 7. check if a year is leap or not.
+
+year = int(input("Enter year :"))
+
+if year > 365:
+    print("leap year")
+else:
+    print("not leap")
+
+
+
+# 8. print "valid password" or "invalid password".
+
+password = (input("Enter your password :"))
+
+if password >= 8:
+    print("valid password")
+if password < 8 :
+    print("invalid password")
+else:
+    ("password must be 8 character")
+
+
+# 9. Determine whether salary is taxtable or not.
+
+salary = int(input("Enter salary :"))
+
+if salary <= 100000:
+    print("salary is average")
+else:
+    print("salary is taxtable")
+
+
+
+# 10. check whether a number is greater than 50 or not.
+
+num = int(input("Enter number :"))
+
+if num > 50:
+    print("Number is greater")
+else:
+    print("Number is less")
+
+
+
+            
+
+    
