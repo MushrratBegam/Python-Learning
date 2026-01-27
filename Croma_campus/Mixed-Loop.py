821a7b2e59439547682135d36fbd9dcc46b661df
mushrrat Begam <mushrratbegam123@gmail.com>
2026-01-27 21:40:00 +0530

    Mixed-(For-While)-Question

--- Croma_campus/Mixed-Loop.py
+++ Croma_campus/Mixed-Loop.py
@@ -1,30 +1,37 @@
 # 1. write a program to print a number pattern using loop.
-# for i in range(1,6):
-#     for j in range(1,i+1):
-#         print(j, end="")
-#     print()
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
 
 
 # 2. write a program to count the frequency of each character in a string.
-
-
-
+text = input("Enter a string :")
+freq = {}
+for char in text:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+print("character frequency :")
+for char, count in freq.items():
+    print(char,":",count)        
 
 
 # 3. write a program to print all armstrong numbers between 1 and 1000.
-# num = 1
-# while num <= 1000:
-#     temporary = num
-#     sum = 0
+num = 1
+while num <= 1000:
+    temporary = num
+    sum = 0
     
-#     while temporary > 0:
-#         digit = temporary % 10
-#         sum = sum + (digit ** 3)
-#         temporary = temporary // 10
+    while temporary > 0:
+        digit = temporary % 10
+        sum = sum + (digit ** 3)
+        temporary = temporary // 10
         
-#     if sum == num:
-#        print(num)
-#     num +=1
+    if sum == num:
+       print(num)
+    num +=1
 
 
 # 4. write a program to simulate an ATM menu using a while loop.
@@ -54,10 +61,10 @@ while True:
         print("Invalid choice")
 
 
-
-
-
-
-
-
 # 5. write a program to find the GCD of two number using loop
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+while b != 0:
+    a,b = b,a % b
+print("GCD", a)  
